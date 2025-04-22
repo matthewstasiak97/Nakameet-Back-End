89003782bb249e62ef0e111a833be796b222cfa1
camiegray <camieg4@gmail.com>
2025-04-22 11:54:44 -0700

    Update

--- models/user.js
+++ models/user.js
@@ -1,8 +1,14 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
-})
+  username: { type: String, required: true },
+  hashedPassword: { type: String, required: true },
+});
 
-export default mongoose.model("User", userSchema)
+userSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    delete returnedObject.hashedPassword;
+  },
+});
+
+export default mongoose.model("User", userSchema);
