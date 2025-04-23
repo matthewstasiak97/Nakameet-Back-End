eb82b7908114d1271c698932f2fc4f9a02cdcbe6
camiegray <camieg4@gmail.com>
2025-04-23 07:15:20 -0700

    Give username creation a unique value

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   hashedPassword: { type: String, required: true },
 });
 
