c220c4956beb23617de66bcc718ee7e30fe6d2a7
camiegray <camieg4@gmail.com>
2025-04-25 11:32:38 -0700

    Fix user routes for proper url

--- routes/users.js
+++ routes/users.js
@@ -5,12 +5,12 @@ import verifyToken from "../middleware/verify-token.js";
 const router = Router();
 
 router.get("/", verifyToken, controllers.getUsers);
-router.get('/:userId', verifyToken, controllers.getUser);
+
 router.post("/sign-up", controllers.signUp);
 router.post("/sign-in", controllers.signIn);
 router.get("/sign-token", controllers.signToken);
 router.post("/sign-out", verifyToken, controllers.signOut);
 router.post("/refresh-token", verifyToken, controllers.refreshToken);
-
+router.get('/:userId', verifyToken, controllers.getUser);
 
 export default router;
